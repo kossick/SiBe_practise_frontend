1764b0a30df56a84e63ef6ce2021070c62b24c70
kossick <tjbullock89@gmail.com>
2021-04-19 11:50:09 +0300

    actually added something!

---
+++ components/QuestionAsker.js
@@ -0,0 +1,39 @@
+app.component('question-asker',
+{
+    template:
+    /*html*/
+    `
+    <h2>{{ questions }}</h2>
+    <p>Will anything print?</p>
+    `,
+    mounted: function () {
+        this.getQuestions()
+    },
+    data() {
+        return{
+            questions: []
+    }
+},
+    methods:{
+        /*async getQuestions() {
+            const response = await fetch("http://localhost:8080/pages/view", {
+                method: 'get',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            const data = await response.json();
+            console.log(typeof(data))
+            this.questions.value = data.parse
+        }  */  
+        async getQuestions() {
+            return fetch("http://localhost:8080/pages/view", {
+                method: 'get',
+                headers: {
+                    'content-type': 'application/json'
+                }
+            })
+                .then(json => {this.questions.value = json.data;})
+        }
+    },
+})
