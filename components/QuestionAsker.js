8138632cac1eafc5e5489a48248f80de31151bf4
kossick <tjbullock89@gmail.com>
2021-04-26 17:25:36 +0300

    Included display of answer at bottom of page when clicked

--- components/QuestionAsker.js
+++ components/QuestionAsker.js
@@ -7,23 +7,30 @@ app.component('question-asker',
         <div class="question">
             <h1>{{ askedQuestion }}</h1>
 
-            <form>
-                <button class="leftButton">{{ optionOne }}</button>
-                <button class="rightButton">{{ optionTwo }}</button>
-            </form>
+            <div class="choices">
+                <button class="leftButton" 
+                 @click="updateAnswer(optionOne)">
+                    {{ optionOne }}
+                </button>
+                <button class="rightButton" 
+                 @click="updateAnswer(optionTwo)">
+                    {{ optionTwo }}
+                </button>
+            </div>
         </div>
         <div class="navBar">
-            <a class="prevQ"><- Previous question</a>
+            <a class="prevQ">&lt- Previous question</a>
             <a class="nextQ">Next question -></a>
-        <div>
+        </div>
     </div>
+    <p class="info" v-if="this.answer">{{ '{Question: ' + askedQuestion + ', Answer: ' + answer + '}' }}</p>
     `,
     mounted: function () {
         this.getQuestions()
     },
     data() {
         return{
-            pageIndex: 0,
+            pageIndex: 1,
             questions: [],
             activeQuestion: '',
             askedQuestion: '',
@@ -34,7 +41,7 @@ app.component('question-asker',
 },
     methods:{ 
         async getQuestions() {
-            console.log('Does this work?')
+            console.log('Does this work?');
             const response = await fetch("http://localhost:8080/questions/view", {
                 method: 'get',
                 mode: 'cors',
@@ -42,11 +49,24 @@ app.component('question-asker',
                     'Content-Type': 'application/json',
                 }
             });
-            this.questions = await response.json()
-            this.activeQuestion = this.questions[this.pageIndex]
+            this.questions = await response.json();
+            this.updateInformation(this.pageIndex);
+        },
+        updateInformation(pageIndex) {
+            this.activeQuestion = this.questions[pageIndex]
             this.askedQuestion = this.activeQuestion.question
             this.optionOne = this.activeQuestion.option_1
             this.optionTwo = this.activeQuestion.option_2
+        },
+        updateAnswer(answer) {
+            let response = {
+                question: this.askedQuestion,
+                answer: answer
+            }
+
+            this.answer = answer;
+
+            console.log(response);
         }
     },
 })
